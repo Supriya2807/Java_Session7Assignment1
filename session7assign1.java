304d50638ca32f7e211ecd666e74a0b634dbcb19
Supriya2807 <lahadesupriya@gmail.com>
2016-06-25 13:49:41 +0530

    session7assign1
    array exception

---
+++ session7assign1.java
@@ -0,0 +1,24 @@
+package acadgild.assignments;
+
+public class session7assign1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try{
+			int [] array = new int[5];
+			
+		array[10]=45;
+			
+						
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			
+			System.out.println("Array Limit is exceeding: "+e);
+			
+			
+			
+		}
+	}
+
+}
